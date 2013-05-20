20e95b7338539641eb112f84fcd8d3ea1e0765f2
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-05-20 17:15:51 +0400

    Added SoundFeatureExtraction submodule
    Change-Id: I4fa1596d45ae36618f74c7ab22733f1257a5fd3d

---
+++ src/Tests/GenreRecognition.py
@@ -0,0 +1,26 @@
+'''
+Created on May 20, 2013
+
+@author: Markovtsev Vadim <v.markovtsev@samsung.com>
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testBuildingWorkflow(self):
+        pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testBuildingWorkflow']
+    unittest.main()
