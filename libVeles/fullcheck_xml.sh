2a30be6f45989e24174b2826d7f8e743aaac1516
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-08-16 10:32:15 +0400

    Blacklisted libarchive and yaml-cpp from checking style
    Change-Id: I94cb9a4440d5835387016874658d9185e6c63ed8

--- libVeles/fullcheck_xml.sh
+++ libVeles/fullcheck_xml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*"  -not -name "config.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
+find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./libarchive/*" -not -path "./yaml-cpp/*" -not -name "config.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
