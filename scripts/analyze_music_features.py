799cca25214d850c556e92ff8e31cfdd368d0899
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-10-17 15:10:47 +0400

    Changed music pickle script to check for nan-s

--- scripts/analyze_music_features.py
+++ scripts/analyze_music_features.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 import pickle
+import numpy
 
 if __name__ == "__main__":
   fr=open('../gtzan.pickle', 'rb')
   root=pickle.load(fr)
   for key, val in root['files'].items():
-       for feat_name, feat_value in val['features'].items():
-               if feat_name == 'Beat':
-                       print("%s: %s" % (key, feat_value['value']))
+      for feat_name, feat_value in val['features'].items():
+          for i in range(0,len(feat_value['value'])):
+              if numpy.isnan(feat_value['value'][i]):
+                  print("%s[%d] - %s" % (key, i, feat_name))
