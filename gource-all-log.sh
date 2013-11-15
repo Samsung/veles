81463203f2f0a3e0465b6cd79cbf05f15ffd2005
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-15 18:15:12 +0400

    Changed help text for gource-all-log.sh
    Former-commit-id: 9ce4029ac184fcdaf9cee4f9121469fa22f539a1

--- gource-all-log.sh
+++ gource-all-log.sh
@@ -4,4 +4,4 @@ git submodule --quiet foreach --recursive '/home/markhor/Development/Veles/gourc
 git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1$sed_path\//g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g' >> history.log
 ./gource-log-fix.py history.log
 rm history.log
-echo "gource --load-config veles.gource --log-format custom"
+echo "gource --load-config veles.gource --log-format custom -s 0.5 --highlight-all-users"
