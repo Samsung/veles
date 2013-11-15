9a7fac8b66b556686258f80189962a1cf501bf58
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-15 17:56:58 +0400

    Added gource scripts
    Former-commit-id: 280ed9bad3faabb9155999d313bd0dd9f98d5047

---
+++ gource-all-log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git submodule --quiet foreach --recursive '/home/markhor/Development/Veles/gource-logger.sh $toplevel $path' > history.log
+git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1$sed_path\//g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g' >> history.log
+./gource-log-fix.py history.log
+rm history.log
+echo "gource --load-config veles.gource --log-format custom"
