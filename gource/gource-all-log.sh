bd57f48480e1a56358d0a8b3b0e4729f86c5ad4d
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-12-11 11:53:45 +0400

    Fixed gource scripts
    Former-commit-id: 0509e45f052be9f2478203fbd70145a2e1223fd1

--- gource/gource-all-log.sh
+++ gource/gource-all-log.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-git submodule --quiet foreach --recursive '/home/markhor/Development/Veles/gource-logger.sh $toplevel $path' > history.log
-git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1$sed_path\//g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g' >> history.log
-./gource-log-fix.py history.log
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+path=$(git rev-parse --show-toplevel)
+git submodule --quiet foreach --recursive "$DIR/gource-logger.sh \$toplevel \$path $path" > history.log
+git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1/g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g' >> history.log
+old_path=$(pwd)
+pushd ../ChannelRecognizer
+git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1ChannelRecognizer\//g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g' >> $old_path/history.log
+popd
+sed -i -e 's/EBulychev/Egor Bulychev/g' -e 's/Markovtsev Vadim/Vadim Markovtsev/g' history.log
+$DIR/gource-log-fix.py history.log
 rm history.log
-echo "gource --load-config veles.gource --log-format custom -s 0.5 --highlight-all-users"
+echo "gource --load-config $DIR/veles.gource --log-format custom -s 0.5 --highlight-all-users"
