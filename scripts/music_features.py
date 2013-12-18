358a31752d642d88325bacddfc7532c6bba91d6e
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-12-18 17:33:01 +0400

    Fixed single threaded mode
    Former-commit-id: f1aa149e62322f443c9c858c263a65a24667a6e5

--- scripts/music_features.py
+++ scripts/music_features.py
@@ -184,7 +184,7 @@ USAGE
         # initialize Library
         Library(library_path)
         # We can do work in parallel more effectively with multiprocessing
-        if not single:
+        if single:
             Library().set_omp_transforms_max_threads_num(1)
         if nosimd:
             Library().set_use_simd(0)
