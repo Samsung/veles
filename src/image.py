f4dfcdea8cd84f359fd8ec0f13181b5ec1470303
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-02-10 12:35:09 +0400

    Revert "Added simple copy when no resize needed"
    This reverts commit 7c528599035117df7471dcc52814b5d99675d7aa [formerly 37548a10c52741f64d64d5aacf3a6bf0ca9b726f].
    
    
    Former-commit-id: 70ae037a8af33d091fa8441b6680832c35ff9bde

--- src/image.py
+++ src/image.py
@@ -3,7 +3,7 @@ Created on Sep 10, 2013
 
 Image Scale.
 
-@author: Kazantsev Alexey <a.kazantsev@samsung.com>
+@author: ajk
 """
 import numpy
 import config
@@ -20,22 +20,12 @@ BILINEAR = 1
 BOX = 2
 
 
-resize_count = 0
-asitis_count = 0
-
-
-def resize(a, width, height, interpolation=BILINEAR, require_copy=False):
+def resize(a, width, height, interpolation=BILINEAR):
     if a.dtype != numpy.uint8:
         raise error.ErrBadFormat("a.dtype should be numpy.uint8")
-    if a.shape[1] == width and a.shape[0] == height:
-        global asitis_count
-        asitis_count += 1
-        return a.copy() if require_copy else a
     b = numpy.zeros([height, width], dtype=a.dtype)
     handle.ScalePlane(a.ctypes.data_as(POINTER(c_byte)),
                       a.shape[1], a.shape[1], a.shape[0],
                       b.ctypes.data_as(POINTER(c_byte)),
                       b.shape[1], b.shape[1], b.shape[0], interpolation)
-    global resize_count
-    resize_count += 1
     return b
