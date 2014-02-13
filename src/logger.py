d1e58d84f8596c69f0dec0b80b4794a8845ca5f2
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-02-13 10:01:18 +0400

    Moved Pickleable from logger.py to units.py
    Former-commit-id: 924540d2d386e23bdd4c28465c22140cd4d571a4

--- src/logger.py
+++ src/logger.py
@@ -21,38 +21,3 @@ class Logger(object):
         """Returns the logger associated with this object.
         """
         return self.logger_
-
-
-class Pickleable(Logger):
-    """Will save attributes ending with _ as None when pickling and will call
-    constructor upon unpickling.
-    """
-    def __init__(self):
-        """Calls init_unpickled() to initialize the attributes which are not
-        pickled.
-        """
-        super(Pickleable, self).__init__()
-        # self.init_unpickled()  # already called in Logger()
-
-    """This function is called if the object has just been unpickled.
-    """
-    def init_unpickled(self):
-        if hasattr(super(Pickleable, self), "init_unpickled"):
-            super(Pickleable, self).init_unpickled()
-
-    def __getstate__(self):
-        """Selects the attributes to pickle.
-        """
-        state = {}
-        for k, v in self.__dict__.items():
-            if k[len(k) - 1] != "_" and not callable(v):
-                state[k] = v
-            else:
-                state[k] = None
-        return state
-
-    def __setstate__(self, state):
-        """Recovers the object after unpickling.
-        """
-        self.__dict__.update(state)
-        self.init_unpickled()
