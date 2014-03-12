b48544307f651b7fa401471ad622ab6fac7fa4f3
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-03-12 17:48:50 +0400

    Fixed launcher parameter for Client and Server
    Former-commit-id: 9ffa1a7a9e6c014608b3b63c8d2227aa4d6bd3ea

--- src/launcher.py
+++ src/launcher.py
@@ -158,7 +158,8 @@ class Launcher(logger.Logger):
         workflow.thread_pool.register_on_shutdown(self._on_shutdown)
 
         if self.is_slave:
-            self.agent = client.Client(self.args.server_address, workflow)
+            self.agent = client.Client(self.args.server_address, workflow,
+                                       self)
         else:
             if self.reports_web_status:
                 self.tornado_ioloop_thread = threading.Thread(
@@ -173,7 +174,8 @@ class Launcher(logger.Logger):
                         workflow.thread_pool, self.matplotlib_backend,
                         self._set_webagg_port)
             if self.is_master:
-                self.agent = server.Server(self.args.listen_address, workflow)
+                self.agent = server.Server(self.args.listen_address, workflow,
+                                           self)
                 # Launch the nodes described in the configuration file/string
                 self._launch_nodes()
             else:
