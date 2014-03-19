5ac70c780cad9f752337809dd5d34d9654fd543f
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-03-19 17:11:15 +0400

    Added some common classes to global imports
    Former-commit-id: 08ebe3d1adf99563838f21be24d8494a95b73e78

--- veles/__init__.py
+++ veles/__init__.py
@@ -10,3 +10,8 @@ from email.utils import parsedate_tz, mktime_tz
 __git__ = "$Commit$"
 __date__ = mktime_tz(parsedate_tz("$Date$"))
 __version__ = "2.0.0"
+
+
+from veles.units import Unit, OpenCLUnit
+from veles.workflows import Workflow, OpenCLWorkflow
+from veles.launcher import Launcher
