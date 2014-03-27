b5b0ba9ca21f2221d0c746e315d8355f80d8ca74
Alexey Kazantsev <a.kazantsev@samsung.com>
2014-03-27 08:48:25 +0400

    Modified HEVC script for gource video creation
    Former-commit-id: 0d918224fdebe59eb76405d4e0335066514dcac0

--- gource/gource-hevc.sh
+++ gource/gource-hevc.sh
@@ -10,6 +10,14 @@ sar=16:9
 preset=slow
 tmp=/tmp/Veles.265
 
+# http://stackoverflow.com/questions/19634453/ffmpeg-how-to-generate-a-mp4-with-h-265-codec
+
+# Via ffmpeg with libx265
+# Example: ./gource-hevc Veles.ppm Veles.mov
+ffmpeg -r $fps -f image2pipe -vcodec ppm -i $1 -an -x265-params crf=$crf -pix_fmt yuv420p $2
+exit
+
+# Via x265
 ffmpeg -r $fps -f image2pipe -vcodec ppm -i $1 -f yuv4mpegpipe -pix_fmt yuv420p - | x265 --y4m --preset $preset --sar $sar --crf $crf --bframes $n_bframes --ref $n_ref --subme $n_subme --keyint $keyint --input-res $res --fps $fps - -o $tmp
 ffmpeg -i $tmp -c copy $2
 # With audio
