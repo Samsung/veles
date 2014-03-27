d5b9c579c2b8dc66d1934f9553748ebd24f6c17e
Alexey Kazantsev <a.kazantsev@samsung.com>
2014-03-27 08:35:23 +0400

    Added muxing for HEVC script for gource video creation
    Former-commit-id: 204eec9778805b758359f8c4bcba7c0af434353e

--- gource/gource-hevc.sh
+++ gource/gource-hevc.sh
@@ -7,5 +7,10 @@ fps=60
 res=1920x1080
 keyint=480
 sar=16:9
+preset=slow
+tmp=/tmp/Veles.265
 
-ffmpeg -r $fps -f image2pipe -vcodec ppm -i $1 -f yuv4mpegpipe -pix_fmt yuv420p - | x265 --y4m --preset slow --sar $sar --crf $crf --bframes $n_bframes --ref $n_ref --subme $n_subme --keyint $keyint --input-res $res --fps $fps - -o $2
+ffmpeg -r $fps -f image2pipe -vcodec ppm -i $1 -f yuv4mpegpipe -pix_fmt yuv420p - | x265 --y4m --preset $preset --sar $sar --crf $crf --bframes $n_bframes --ref $n_ref --subme $n_subme --keyint $keyint --input-res $res --fps $fps - -o $tmp
+ffmpeg -i $tmp -c copy $2
+# With audio
+#ffmpeg -i $tmp -i INPUT_AUDIO -map 0 -map 1:a -c copy $2
