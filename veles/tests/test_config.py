6f3d60a80c0a396e65a4e0921f81b210dd38510f
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-03-31 16:53:33 +0400

    Added unit-test for config
    Change-Id: I15d43876fc986894b6021f72110c22d6bf4312ed
    
    Former-commit-id: 2def127cc23cefa36a5cf368238f49678cb6bd45

---
+++ veles/tests/test_config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3.3 -O
+"""
+Created on November 18, 2013
+
+@author: Lyubov Podoynitsina <lyubov.p@samsung.com>
+"""
+
+
+import logging
+import unittest
+
+from veles.config import root, Config
+
+
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        root.common.unit_test = True
+
+    def test_temp_config(self):
+        logging.info("Will test automatic appearance of an"
+                     " intermediate Config object")
+        test = Config()
+        test.test_.test_value = 5
+        max_diff = test.test_.test_value - 5
+        self.assertLess(max_diff, 0.0001,
+                        "Result differs by %.6f" % (max_diff))
+        logging.info("All ok")
+
+    def test_update_multi_line(self):
+        logging.info("Will test multi-line update function")
+        test = Config()
+
+        test.test_value = 0.01
+        test.test_.test_value = 5
+        test.test_.test_str = "Test"
+
+        test.update = {"test_value": 0.5,
+                       "test_": {"test_value": 12,
+                                 "test_str": "new test"}
+                       }
+        self.assertEqual(test.test_value, 0.5,
+                         "Not equal values on first level")
+        self.assertEqual(test.test_.test_value, 12,
+                         "Not equal values on second level")
+        self.assertEqual(test.test_.test_str, "new test", "Not equal strings")
+
+        logging.info("All Ok")
+
+    def test_update_one_line(self):
+        logging.info("Will test one-line update function")
+        test = Config()
+
+        test.update = {"test_value": 0.5,
+                       "test_": {"test_value": 12,
+                                 "test_str": "new test"}
+                       }
+
+        test.test_value = 0.01
+        test.test_.test_value = 5
+        test.test_.test_str = "Test"
+
+        self.assertEqual(test.test_value, 0.01,
+                         "Not equal values on first level")
+        self.assertEqual(test.test_.test_value, 5,
+                         "Not equal values on second level")
+        self.assertEqual(test.test_.test_str, "Test", "Not equal strings")
+
+        logging.info("All Ok")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("Will test config unit")
+    unittest.main()
