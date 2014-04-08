c0e24bfbdf26d39ee08f81ef81207ae75a0d15f4
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-08 13:02:57 +0400

    Added Shell unit to enable interactive cmdline during workflow execution
    Change-Id: I895e488a8e27f6fcf39b237037654425f96e5dd3
    
    Former-commit-id: ea8dce6a09c842857171acd7c3f2bab695b69c10

---
+++ veles/interaction.py
@@ -0,0 +1,45 @@
+"""
+Created on Apr 7, 2014
+
+@author: Vadim Markovtsev <v.markovtsev@samsung.com>
+"""
+
+
+from IPython.config.loader import Config
+from IPython.terminal.embed import InteractiveShellEmbed
+import select
+import sys
+
+from veles.units import Unit
+
+
+class Shell(Unit):
+    """
+    Runs embedded IPython
+    """
+    BANNER1 = "\nVELES interactive console"
+    BANNER2 = "Type in 'workflow' or 'units' to start"
+
+    def __init__(self, workflow, **kwargs):
+        super(Shell, self).__init__(workflow, **kwargs)
+        self.cfg = Config()
+        self.cfg.PromptManager.in_template = "veles [\\#]> "
+        self.cfg.PromptManager.out_template = "veles [\\#]: "
+        self.cfg.HistoryManager.enabled = False
+
+    def initialize(self):
+        super(Shell, self).initialize()
+        self.shell_ = InteractiveShellEmbed(config=self.cfg,
+                                            banner1=Shell.BANNER1,
+                                            banner2=Shell.BANNER2)
+
+    def run(self):
+        key = ''
+        i, _, _ = select.select([sys.stdin], [], [], 0)
+        for s in i:
+            if s == sys.stdin:
+                key = sys.stdin.readline()[0]
+        if key == 'i':
+            workflow = self.workflow  # pylint: disable=W0612
+            units = self.workflow.units  # pylint: disable=W0612
+            self.shell_()
