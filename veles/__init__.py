3ba9a6bdd4fb5e7906eea65da65c448e7fae0eec
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-14 14:08:42 +0400

    Bumped version to 0.3
    Change-Id: Iadbf0704d2b80959b58a487fd69b0b7e40a38086
    
    Former-commit-id: 91e256e63776d3ef03d132a2ad2f8d19b0c7f78f

--- veles/__init__.py
+++ veles/__init__.py
@@ -11,7 +11,7 @@ from email.utils import parsedate_tz, mktime_tz
 
 __git__ = "$Commit$"
 __date__ = mktime_tz(parsedate_tz("$Date$"))
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __license__ = "Samsung Proprietary License"
 __copyright__ = "© 2013 Samsung Electronics Co., Ltd."
 
