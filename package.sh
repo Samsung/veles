79817eac47f97abec235d4aca747b5496c1d33e8
Alexey Golovizin <a.golovizin@samsung.com>
2014-04-15 13:54:47 +0400

    Added veles/__init__.py filter workaround
    Change-Id: Ib848bda106d5cdd5146014f8d325ba6e11396f71
    
    Former-commit-id: 40b2ed2577a9dc7aac381d91c8579e3dd14c9fb6

--- package.sh
+++ package.sh
@@ -4,6 +4,7 @@ if test "$1" = "smudge"; then
   commit=$(git rev-parse HEAD)
   date=$(git log --pretty=format:"%ad" --date=rfc2822 -1)
   sed -e "s/\\\$Commit\\\$/$commit/g" -e "s/\\\$Date\\\$/$date/g" <&0
+  cat <&0
 elif test "$1" = "clean"; then
-  sed -e "s/\(__git__ = \).*/\\1\"\\\$Commit\\\$\"/g" -e "s/\\(__date__ = mktime_tz(parsedate_tz(\"\\).*/\\1\\\$Date\\\$\"))/g" <&0
+  sed -e "s/\(__git__ = \"\).*/\\1\\\$Commit\\\$\"/g" -e "s/\\(__date__ = mktime_tz(parsedate_tz(\"\\).*/\\1\\\$Date\\\$\"))/g" <&0
 fi
