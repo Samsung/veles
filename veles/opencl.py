a3597c699757c6b6936e548e49b5688d38735134
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-25 13:32:07 +0400

    Extracted OpenCLUnit and OpenCLWorkflow into the separate module
    Change-Id: I2d4000c1336202005e9e3f5bfb5bc5c0bff58297
    
    Former-commit-id: 9e143785ff832e06901f40d5e335b4432805a93a

--- veles/opencl.py
+++ veles/opencl.py
@@ -8,7 +8,6 @@ Copyright (c) 2013 Samsung Electronics Co., Ltd.
 
 
 import argparse
-from copy import copy
 import numpy
 import os
 import six
@@ -23,7 +22,6 @@ import veles.formats as formats
 import veles.opencl_types as opencl_types
 import veles.rnd as rnd
 import veles.units as units
-import veles.workflows as workflows
 import veles.external.prettytable as prettytable
 
 
@@ -404,6 +402,8 @@ class Device(units.Pickleable):
             def del_ref(self, unit):
                 pass
 
+        from veles.opencl_units import OpenCLUnit
+
         obj = OpenCLUnit(WorkflowStub(), device=self)
         obj.cl_sources_["forward.cl"] = {}
         defines = {
@@ -449,168 +449,3 @@ class Device(units.Pickleable):
         # Get results back
         self.c.map_read()
         return dt / iters
-
-
-class OpenCLUnit(units.Unit):
-    """Unit that operates using OpenCL.
-
-    Attributes:
-        device: Device object.
-        prg_: OpenCL program.
-        cl_sources: OpenCL source files: file => defines.
-        prg_src: last built OpenCL program source code text.
-    """
-    def __init__(self, workflow, **kwargs):
-        device = kwargs.get("device")
-        kwargs["device"] = device
-        super(OpenCLUnit, self).__init__(workflow, **kwargs)
-        self.device = device
-
-    def init_unpickled(self):
-        super(OpenCLUnit, self).init_unpickled()
-        self.prg_ = None
-        self.cl_sources_ = {}
-
-    def cpu_run(self):
-        """Run on CPU only.
-        """
-        return super(OpenCLUnit, self).run()
-
-    def ocl_run(self):
-        """Run on GPU/any OpenCL capable device.
-        """
-        return self.cpu_run()
-
-    def initialize(self, device=None):
-        super(OpenCLUnit, self).initialize()
-        if device is not None:
-            self.device = device
-        elif hasattr(self.workflow, "device"):
-            self.device = self.workflow.device
-
-    def run(self):
-        t1 = time.time()
-        if self.device:
-            self.ocl_run()
-        else:
-            self.cpu_run()
-        self.debug("%s in %.2f sec" %
-                   (self.__class__.__name__, time.time() - t1))
-
-    def build_program(self, defines=None, dump_filename=None, dtype=None):
-        """Builds OpenCL program.
-
-        prg_ will be initialized to the built program.
-        """
-        if defines and not isinstance(defines, dict):
-            raise RuntimeError("defines must be a dictionary")
-        lines = []
-        my_defines = copy(defines) if defines else {}
-        for fnme, defs in self.cl_sources_.items():
-            lines.append("#include \"%s\"" % (fnme))
-            my_defines.update(defs)
-        if dtype is None:
-            dtype = root.common.precision_type
-        elif type(dtype) != str:
-            dtype = opencl_types.numpy_dtype_to_opencl(dtype)
-        my_defines.update(opencl_types.cl_defines[dtype])
-
-        for k, v in my_defines.items():
-            lines.insert(0, "#define %s %s" % (k, v))
-
-        source = "\n".join(lines)
-
-        try:
-            self.prg_ = self.device.queue_.context.create_program(
-                source, root.common.ocl_dirs)
-            if len(self.prg_.build_logs):
-                for s in self.prg_.build_logs:
-                    s = s.strip()
-                    if not len(s):
-                        continue
-                    self.info("Non-empty OpenCL build log encountered: %s", s)
-        finally:
-            if dump_filename is not None:
-                flog = open(dump_filename, "w")
-                flog.write(source)
-                flog.close()
-
-    def get_kernel(self, name):
-        return self.prg_.get_kernel(name)
-
-    def execute_kernel(self, krn, global_size, local_size):
-        return self.device.queue_.execute_kernel(krn, global_size, local_size)
-
-
-class OpenCLBenchmark(OpenCLUnit):
-    """
-    Executes an OpenCL benchmark to estimate the computing power.
-    """
-
-    def __init__(self, workflow, **kwargs):
-        device = kwargs.get("device")
-        if device is None:
-            device = Device()
-        kwargs["device"] = device
-        super(OpenCLBenchmark, self).__init__(workflow, **kwargs)
-        self.block_size = 30
-        self.size = 3000
-        self.cl_sources_ = {"benchmark.cl": {
-            'BLOCK_SIZE': self.block_size,
-            'SIZE': self.size
-        }}
-        self.build_program()
-        self.kernel_ = self.get_kernel("benchmark")
-        msize = [self.size, self.size]
-        self.input_A_ = formats.Vector()
-        self.input_B_ = formats.Vector()
-        self.output_C_ = formats.Vector()
-        self.input_A_.v = numpy.zeros(msize, dtype=numpy.double)
-        self.input_B_.v = numpy.zeros(msize, dtype=numpy.double)
-        self.output_C_.v = numpy.zeros(msize, dtype=numpy.double)
-        self.input_A_.initialize(device)
-        self.input_B_.initialize(device)
-        self.output_C_.initialize(device)
-        self.kernel_.set_arg(0, self.input_A_.v_)
-        self.kernel_.set_arg(1, self.input_B_.v_)
-        self.kernel_.set_arg(2, self.output_C_.v_)
-
-    def estimate(self):
-        """
-        Launches and waits for the benchmark to finish being executed.
-        """
-        global_size = [formats.roundup(self.size, self.block_size),
-                       formats.roundup(self.size, self.block_size)]
-        local_size = [self.block_size, self.block_size]
-        tstart = time.time()
-        event = self.device.queue_.execute_kernel(self.kernel_, global_size,
-                                                  local_size)
-        event.wait()
-        self.output_C_.map_read()
-        tfinish = time.time()
-        delta = tfinish - tstart
-        return 1000 / delta
-
-
-class OpenCLWorkflow(OpenCLUnit, workflows.Workflow):
-    """Base class for OpenCL workflows
-    """
-
-    def initialize(self, device=None):
-        if device is not None:
-            self.device = device
-        super(OpenCLWorkflow, self).initialize()
-        self._power = None
-
-    @property
-    def computing_power(self):
-        """
-        Estimates this slave's computing power for initial perfect balancing.
-        Run by a slave.
-        """
-        if not self._power:
-            bench = OpenCLBenchmark(self, device=self.device)
-            self._power = bench.estimate()
-            self.del_ref(bench)
-            self.info("Computing power is %.6f", self._power)
-        return self._power
---
+++ veles/opencl_units.py
@@ -0,0 +1,175 @@
+'''
+Created on Apr 25, 2014
+
+Copyright (c) 2014, Samsung Electronics, Co., Ltd.
+'''
+
+
+from copy import copy
+import numpy
+import time
+
+from veles.config import root
+import veles.formats as formats
+import veles.opencl_types as opencl_types
+import veles.units as units
+import veles.workflows as workflows
+
+
+class OpenCLUnit(units.Unit):
+    """Unit that operates using OpenCL.
+
+    Attributes:
+        device: Device object.
+        program_: OpenCL program.
+        cl_sources: OpenCL source files: file => defines.
+        prg_src: last built OpenCL program source code text.
+    """
+    def __init__(self, workflow, **kwargs):
+        super(OpenCLUnit, self).__init__(workflow, **kwargs)
+        self.device = kwargs.get("device")
+
+    def init_unpickled(self):
+        super(OpenCLUnit, self).init_unpickled()
+        self.program_ = None
+        self.cl_sources_ = {}
+
+    def cpu_run(self):
+        """Run on CPU only.
+        """
+        return super(OpenCLUnit, self).run()
+
+    def ocl_run(self):
+        """Run on GPU/any OpenCL capable device.
+        """
+        return self.cpu_run()
+
+    def initialize(self, device=None):
+        super(OpenCLUnit, self).initialize()
+        if device is not None:
+            self.device = device
+        elif hasattr(self.workflow, "device"):
+            self.device = self.workflow.device
+
+    def run(self):
+        t1 = time.time()
+        if self.device:
+            self.ocl_run()
+        else:
+            self.cpu_run()
+        self.debug("%s in %.2f sec" %
+                   (self.__class__.__name__, time.time() - t1))
+
+    def build_program(self, defines=None, dump_filename=None, dtype=None):
+        """Builds OpenCL program.
+
+        program_ will be initialized to the built program.
+        """
+        if defines and not isinstance(defines, dict):
+            raise RuntimeError("defines must be a dictionary")
+        lines = []
+        my_defines = copy(defines) if defines else {}
+        for fnme, defs in self.cl_sources_.items():
+            lines.append("#include \"%s\"" % (fnme))
+            my_defines.update(defs)
+        if dtype is None:
+            dtype = root.common.precision_type
+        elif type(dtype) != str:
+            dtype = opencl_types.numpy_dtype_to_opencl(dtype)
+        my_defines.update(opencl_types.cl_defines[dtype])
+
+        for k, v in my_defines.items():
+            lines.insert(0, "#define %s %s" % (k, v))
+
+        source = "\n".join(lines)
+
+        try:
+            self.program_ = self.device.queue_.context.create_program(
+                source, root.common.ocl_dirs)
+            if len(self.program_.build_logs):
+                for s in self.program_.build_logs:
+                    s = s.strip()
+                    if not len(s):
+                        continue
+                    self.info("Non-empty OpenCL build log encountered: %s", s)
+        finally:
+            if dump_filename is not None:
+                flog = open(dump_filename, "w")
+                flog.write(source)
+                flog.close()
+
+    def get_kernel(self, name):
+        return self.program_.get_kernel(name)
+
+    def execute_kernel(self, krn, global_size, local_size):
+        return self.device.queue_.execute_kernel(krn, global_size, local_size)
+
+
+class OpenCLBenchmark(OpenCLUnit):
+    """
+    Executes an OpenCL benchmark to estimate the computing power.
+    """
+
+    def __init__(self, workflow, **kwargs):
+        super(OpenCLBenchmark, self).__init__(workflow, **kwargs)
+        self.block_size = 30
+        self.size = 3000
+        self.cl_sources_ = {"benchmark.cl": {
+            'BLOCK_SIZE': self.block_size,
+            'SIZE': self.size
+        }}
+        self.build_program()
+        self.kernel_ = self.get_kernel("benchmark")
+        msize = [self.size, self.size]
+        self.input_A_ = formats.Vector()
+        self.input_B_ = formats.Vector()
+        self.output_C_ = formats.Vector()
+        self.input_A_.v = numpy.zeros(msize, dtype=numpy.double)
+        self.input_B_.v = numpy.zeros(msize, dtype=numpy.double)
+        self.output_C_.v = numpy.zeros(msize, dtype=numpy.double)
+        self.input_A_.initialize(self.device)
+        self.input_B_.initialize(self.device)
+        self.output_C_.initialize(self.device)
+        self.kernel_.set_arg(0, self.input_A_.v_)
+        self.kernel_.set_arg(1, self.input_B_.v_)
+        self.kernel_.set_arg(2, self.output_C_.v_)
+
+    def estimate(self):
+        """
+        Launches and waits for the benchmark to finish being executed.
+        """
+        global_size = [formats.roundup(self.size, self.block_size),
+                       formats.roundup(self.size, self.block_size)]
+        local_size = [self.block_size, self.block_size]
+        tstart = time.time()
+        event = self.device.queue_.execute_kernel(self.kernel_, global_size,
+                                                  local_size)
+        event.wait()
+        self.output_C_.map_read()
+        tfinish = time.time()
+        delta = tfinish - tstart
+        return 1000 / delta
+
+
+class OpenCLWorkflow(OpenCLUnit, workflows.Workflow):
+    """Base class for OpenCL workflows
+    """
+
+    def initialize(self, device=None):
+        if device is not None:
+            self.device = device
+        super(OpenCLWorkflow, self).initialize()
+        self._power = None
+
+    @property
+    def computing_power(self):
+        """
+        Estimates this slave's computing power for initial perfect balancing.
+        Run by a slave.
+        """
+        if not self._power:
+            bench = OpenCLBenchmark(self, device=self.device)
+            self._power = bench.estimate()
+            self.del_ref(bench)
+            self.info("Computing power is %.6f", self._power)
+        return self._power
