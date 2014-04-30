ea032c1c05b9803f3a547bc06a8b3f4fa8e7d2cf
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-30 18:45:38 +0400

    Added binary OpenCL caching
    Change-Id: Ifb14c488cc11b73fa2e4e8f7d74ef20baa775b79
    
    Former-commit-id: 7a623c22d95e85814325fd79b1c1cb17d70716a3

--- veles/opencl_units.py
+++ veles/opencl_units.py
@@ -7,6 +7,10 @@ Copyright (c) 2014, Samsung Electronics, Co., Ltd.
 
 from copy import copy
 import numpy
+import os
+from six.moves import cPickle as pickle
+from six import BytesIO
+import tarfile
 import time
 
 from veles.config import root
@@ -22,17 +26,26 @@ class OpenCLUnit(units.Unit):
     Attributes:
         device: Device object.
         program_: OpenCL program.
-        cl_sources: OpenCL source files: file => defines.
-        prg_src: last built OpenCL program source code text.
+        cl_sources_: OpenCL source files: file => defines.
+        cache: whether to cache the compiled OpenCL programs.
     """
     def __init__(self, workflow, **kwargs):
         super(OpenCLUnit, self).__init__(workflow, **kwargs)
         self.device = None
+        self._cache = kwargs.get("cache", True)
 
     def init_unpickled(self):
         super(OpenCLUnit, self).init_unpickled()
         self.program_ = None
         self.cl_sources_ = {}
+
+    @property
+    def cache(self):
+        return self._cache
+
+    @cache.setter
+    def cache(self, value):
+        self._cache = value
 
     def initialize(self, device, **kwargs):
         super(OpenCLUnit, self).initialize(device=device, **kwargs)
@@ -54,11 +67,36 @@ class OpenCLUnit(units.Unit):
         """
         return self.cpu_run()
 
-    def build_program(self, defines=None, dump_filename=None, dtype=None):
+    def build_program(self, defines=None, cache_file_name=None, dtype=None):
         """Builds the OpenCL program.
 
         program_ will be initialized to the resulting program object.
         """
+        if self.cache and os.path.exists("%s.cache" % cache_file_name):
+            binaries = self._load_from_cache(cache_file_name, defines, dtype)
+            if binaries is not None:
+                self.program_ = self.device.queue_.context.create_program(
+                    binaries, binary=True)
+                self.debug("Used %s.cache", cache_file_name)
+                return
+        source = self._generate_source(defines, dtype)
+        self.program_ = self.device.queue_.context.create_program(
+            source, root.common.ocl_dirs)
+        if len(self.program_.build_logs):
+            for s in self.program_.build_logs:
+                s = s.strip()
+                if not len(s):
+                    continue
+                self.info("Non-empty OpenCL build log encountered: %s", s)
+        self._save_to_cache(cache_file_name)
+
+    def get_kernel(self, name):
+        return self.program_.get_kernel(name)
+
+    def execute_kernel(self, krn, global_size, local_size):
+        return self.device.queue_.execute_kernel(krn, global_size, local_size)
+
+    def _generate_source(self, defines, dtype=None):
         if defines and not isinstance(defines, dict):
             raise RuntimeError("defines must be a dictionary")
         lines = []
@@ -72,31 +110,45 @@ class OpenCLUnit(units.Unit):
             dtype = opencl_types.numpy_dtype_to_opencl(dtype)
         my_defines.update(opencl_types.cl_defines[dtype])
 
-        for k, v in my_defines.items():
+        for k, v in sorted(my_defines.items()):
             lines.insert(0, "#define %s %s" % (k, v))
 
         source = "\n".join(lines)
+        return source
 
-        try:
-            self.program_ = self.device.queue_.context.create_program(
-                source, root.common.ocl_dirs)
-            if len(self.program_.build_logs):
-                for s in self.program_.build_logs:
-                    s = s.strip()
-                    if not len(s):
-                        continue
-                    self.info("Non-empty OpenCL build log encountered: %s", s)
-        finally:
-            if dump_filename is not None:
-                flog = open(dump_filename, "w")
-                flog.write(source)
-                flog.close()
+    def _load_from_cache(self, cache_file_name, defines, dtype):
+        with tarfile.open("%s.cache" % cache_file_name, "r") as tar:
+            cached_source = tar.extractfile("source.cl").read()
+            real_source = self._generate_source(defines, dtype).encode()
+            if cached_source != real_source:
+                return None
+            cache = pickle.loads(tar.extractfile("binaries.pickle").read())
+            if (self.device.queue_.device.name != cache["devices"][0][0] or
+                self.device.queue_.device.platform.name !=
+                    cache["devices"][0][1]):
+                return None
+            return cache["binaries"]
 
-    def get_kernel(self, name):
-        return self.program_.get_kernel(name)
-
-    def execute_kernel(self, krn, global_size, local_size):
-        return self.device.queue_.execute_kernel(krn, global_size, local_size)
+    def _save_to_cache(self, cache_file_name):
+        with tarfile.open("%s.cache" % cache_file_name, "w") as tar:
+            source_io = BytesIO()
+            source_io.write(self.program_.source)
+            ti = tarfile.TarInfo("source.cl")
+            ti.size = source_io.tell()
+            ti.mode = int("666", 8)
+            source_io.seek(0)
+            tar.addfile(ti, fileobj=source_io)
+            binaries_io = BytesIO()
+            pickler = pickle.Pickler(binaries_io)
+            binaries = {"binaries": self.program_.binaries,
+                        "devices": [(d.name, d.platform.name)
+                                    for d in self.program_.devices]}
+            pickler.dump(binaries)
+            ti = tarfile.TarInfo("binaries.pickle")
+            ti.size = binaries_io.tell()
+            ti.mode = int("666", 8)
+            binaries_io.seek(0)
+            tar.addfile(ti, fileobj=binaries_io)
 
 
 class OpenCLBenchmark(OpenCLUnit):
