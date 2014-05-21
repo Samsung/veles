63daaaec07d0b73bd14049329c132dfba43628d1
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-05-21 17:13:21 +0400

    Added timeout decorator for finite tests
    Change-Id: I477d0e87411ddcde373a9b1dbf569bf4a1ac32dd
    
    Former-commit-id: 2fa4c64c0a37f4b534e3f971b9bed6eb2cf9f16d

--- veles/tests/__init__.py
+++ veles/tests/__init__.py
@@ -6,3 +6,4 @@ Copyright (c) 2013 Samsung Electronics Co., Ltd.
 
 
 from veles.tests.dummy_workflow import DummyWorkflow
+from veles.tests.timeout import timeout
---
+++ veles/tests/timeout.py
@@ -0,0 +1,57 @@
+"""
+Created on May 21, 2014
+
+Copyright (c) 2014, Samsung Electronics, Co., Ltd.
+"""
+
+
+import os
+import signal
+import sys
+import threading
+
+
+new_event = threading.Event()
+thread_args = None
+
+
+def wait():
+    while new_event.wait():
+        new_event.clear()
+        event, seconds = thread_args
+        if event is None:
+            return
+        if not event.wait(seconds):
+            os.kill(os.getpid(), signal.SIGINT)
+
+
+thread = threading.Thread(target=wait)
+thread.start()
+sysexit = sys.exit
+
+
+def shutdown(errcode=0):
+    global thread_args
+    thread_args = (None, None)
+    new_event.set()
+    if thread.is_alive():
+        thread.join()
+    sysexit(errcode)
+
+
+sys.exit = shutdown
+
+
+def timeout(value=60):
+    def timeout_impl(fn):
+        def wrapped(*args, **kwargs):
+            event = threading.Event()
+            global thread_args
+            thread_args = (event, value)
+            new_event.set()
+            res = fn(*args, **kwargs)
+            event.set()
+            return res
+
+        return wrapped
+    return timeout_impl
