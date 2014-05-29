a6d3ef952bff3de951c3af4a42c59c327b9f782b
Alexey Kazantsev <a.kazantsev@samsung.com>
2014-05-29 14:23:13 +0400

    Extended test_mutable.py
    Former-commit-id: 9206be57afdf6374e38885ee0c286cea3e77b491

--- veles/tests/test_mutable.py
+++ veles/tests/test_mutable.py
@@ -86,13 +86,53 @@ class Test(unittest.TestCase):
         self.assertRaises(ValueError,
                           LinkableAttribute(b, "number", (b, "number")))
 
-    def testLinkableAttribute2(self):
+    def testLinkableAttribute100(self):
+        a = A()
+        a.number = 77
+        b = []
+        bb = []
+        bbb = []
+        for i in range(100):
+            b.append(B())
+            LinkableAttribute(b[i], "number", (a, "number"))
+            bb.append(B())
+            LinkableAttribute(bb[i], "number", (b[i], "number"))
+            bbb.append(B())
+            LinkableAttribute(bbb[i], "number",
+                              (bbb[i - 1] if i else bb[i], "number"))
+        a.number = 123
+        for i in range(100):
+            self.assertEqual(b[i].number, 123)
+            self.assertEqual(bb[i].number, 123)
+            self.assertEqual(bbb[i].number, 123)
+
+    def testLinkableAttributeConstructorAssignment(self):
         a = A()
         a.number = 77
         c = C()
         LinkableAttribute(c, "number", (a, "number"))
-        c2 = C()
+        c2 = C()  # exception should not be here
         LinkableAttribute(c2, "number", (a, "number"))
+
+    def testLinkableAttributeConstructorAssignment100(self):
+        a = A()
+        a.number = 77
+        c = []
+        cc = []
+        ccc = []
+        for i in range(100):
+            c.append(C())
+            LinkableAttribute(c[i], "number", (a, "number"))
+            cc.append(C())
+            LinkableAttribute(cc[i], "number", (c[i], "number"))
+            ccc.append(B())
+            LinkableAttribute(ccc[i], "number",
+                              (ccc[i - 1] if i else cc[i], "number"))
+        a.number = 123
+        for i in range(100):
+            self.assertEqual(c[i].number, 123)
+            self.assertEqual(cc[i].number, 123)
+            self.assertEqual(ccc[i].number, 123)
 
 
 if __name__ == "__main__":
