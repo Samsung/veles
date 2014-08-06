da618f57e5cd64363e260d03c5d598d65aa69c44
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-08-06 15:01:02 +0400

    modified script for parallel training of imagenet
    Change-Id: I8cb883f7acf9c26db4006bdba34f7f0cf5707ef2
    
    Former-commit-id: 4a6a18317b855057dd2d0bee3564a2c01f3fce4e

--- 1000.sh
+++ 1000.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 echo Folder: $1
 dev=$2
-#nodes=markovtsevu64/1:0,kuznetsovu64/0:0,seresovu64/0:0,kazantsevu64/0:0,golovizinu64/1:0,lpod/1:0,seninu64/1:0,smaug/0:0-1,smaug/1:0-1,intern23/0:0
-nodes=lpod/1:0,
-script="scripts/velescli.py -d $dev -l 0.0.0.0:5000 -n $nodes --manhole --debug NNSnapshotter,Snapshotter -f /tmp/1000.log -p '' "
-snapshot="-w /data/veles/datasets/imagenet/snapshots/"$1"/imagenet_ae_"$1"_parallel_current.3.pickle"
-workflow="veles/znicz/tests/research/imagenet/imagenet_ae.py veles/znicz/tests/research/imagenet/imagenet_ae_temp_paral_config.py"
+nodes=markovtsevu64/1:0,kuznetsovu64/0:0,seresovu64/0:0,kazantsevu64/0:0,golovizinu64/1:0,lpod/1:0,seninu64/1:0,smaug/0:0-1,intern23/0:0
+#nodes=seresovu64/0:0,kazantsevu64/0:0,golovizinu64/1:0,lpod/1:0,seninu64/1:0,smaug/0:0-1,intern23/0:0
+#nodes=smaug/1:0-1,smaug/0:0-1
+export NO_LOG=1
+script="scripts/velescli.py -d $dev -l 0.0.0.0:5001 -n $nodes --manhole --debug NNSnapshotter,Snapshotter,Client,Server -f /tmp/1000.log -p '' "
+snapshot="-w /data/veles/datasets/imagenet/snapshots/"$1"/imagenet_ae_"$1"_current.3.pickle"
+workflow="veles/znicz/tests/research/imagenet/imagenet_ae.py veles/znicz/tests/research/imagenet/imagenet_ae_1000_config.py"
 params="root.loader.year=\""$1"\" root.common.plotters_disabled=True"
 cmd0=""$script" "$workflow" "$params
 cmd1=""$script" "$snapshot" "$workflow" "$params
-delay=180
+delay=600
 
-echo "Will execute:"
-echo $cmd0
-if ! $cmd0; then
-  echo "Failed, exit"
-  exit
-fi
+#echo "Will execute:"
+#echo $cmd0
+#if ! $cmd0; then
+#  echo "Failed, exit"
+#  exit
+#fi
 
 echo "Will train next layer after "$delay" seconds"
 sleep $delay
