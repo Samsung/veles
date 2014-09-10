24889495c90a4449fa811660b1fbefba67e50385
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-09-10 15:01:46 +0400

    Modified docs/generate_docs.py
    Change-Id: I5a1707a20953b715e1ac0e174447c06688f56e21
    
    Former-commit-id: 60ce8ab4c85984412d01e374619397a6c2533396

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -10,18 +10,20 @@ import os
 import subprocess
 import sys
 
-docs_path = os.path.dirname(os.path.realpath(__file__))
-docs_source_path = os.path.join(docs_path, "source")
-project_path = os.path.realpath(os.path.join(docs_path, ".."))
 
-sys.path.append(docs_path)
+if __name__ == "__main__":
+    docs_path = os.path.dirname(os.path.realpath(__file__))
+    docs_source_path = os.path.join(docs_path, "source")
+    project_path = os.path.realpath(os.path.join(docs_path, ".."))
 
-subprocess.call(["sphinx-apidoc", "-e", "-f", "-H",
-                "Veles Machine Learning Platform", "-o", docs_source_path,
-                project_path, os.path.join(project_path, "veles/external"),
-                os.path.join(project_path, "veles/znicz/external"),
-                os.path.join(project_path, "veles/tests"),
-                os.path.join(project_path, "veles/znicz/tests")])
-subprocess.call(["sphinx-build", "-b", "html", "-d",
-                 os.path.join(docs_path, "build/doctrees"),
-                 docs_source_path, os.path.join(docs_path, "build/html")])
+    sys.path.append(docs_path)
+
+    subprocess.call(["sphinx-apidoc", "-e", "-f", "-H",
+                    "Veles Machine Learning Platform", "-o", docs_source_path,
+                    project_path, os.path.join(project_path, "veles/external"),
+                    os.path.join(project_path, "veles/znicz/external"),
+                    os.path.join(project_path, "veles/tests"),
+                    os.path.join(project_path, "veles/znicz/tests")])
+    subprocess.call(["sphinx-build", "-b", "html", "-d",
+                     os.path.join(docs_path, "build/doctrees"),
+                     docs_source_path, os.path.join(docs_path, "build/html")])
