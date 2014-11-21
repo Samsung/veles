cdc14ea906d3f9d9f68db5e9b746ec62a6504c3b
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-11-21 18:34:29 +0300

    Bumped core version to 0.5.1
    Change-Id: I1854955b0ed1253f566b8c502f75b902cfba1cea
    
    Former-commit-id: cea0325208b13c72b27f39c60bb3484ddde23db6

--- veles/__init__.py
+++ veles/__init__.py
@@ -18,7 +18,7 @@ from warnings import warn
 from veles.paths import __root__
 
 __project__ = "Veles Machine Learning Platform"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __license__ = "Samsung Proprietary License"
 __copyright__ = u"© 2013 Samsung Electronics Co., Ltd."
 __authors__ = ["Gennady Kuznetsov", "Vadim Markovtsev", "Alexey Kazantsev",
