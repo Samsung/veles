c6a2e4f68604f812d5491e8d7a925e9d5c9a79c5
Dmitry Senin <d.senin@samsung.com>
2014-12-09 13:16:47 +0300

    Added defines.cu and highlight.cuh
    Change-Id: Ib46de4d2cf92417a93ec21d26d799853f3dbf5fc
    
    Former-commit-id: 5ff3c277c9dff696d87cb59a10a3fb53434dbde2

---
+++ cuda/defines.cu
@@ -0,0 +1,45 @@
+#ifndef _DEFINES_
+#define _DEFINES_
+
+#include <highlight.cuh>
+
+#ifndef dtype
+#define dtype int
+#endif
+
+#ifndef FLT_MAX
+#define FLT_MAX 1E+37
+#endif
+
+/**
+ * @brief CUDA implementation of atomicAdd for double.
+ *
+ * CUDA already has following implementations:
+ * - int atomicAdd(int* address, int val);
+ * - unsigned int atomicAdd(unsigned int* address, unsigned int val);
+ * - unsigned long long int atomicAdd(unsigned long long int* address,
+ *                                    unsigned long long int val);
+ * - float atomicAdd(float* address, float val);
+ */
+__device__ double atomicAdd(double* address, double val) {
+  unsigned long long int* address_as_ull =
+  (unsigned long long int*)address;
+  unsigned long long int old = *address_as_ull, assumed;
+  do {
+    assumed = old;
+    old = atomicCAS(address_as_ull, assumed,
+        __double_as_longlong(val +
+            __longlong_as_double(assumed)));
+  } while (assumed != old);
+  return __longlong_as_double(old);
+}
+
+/// @brief Sets all elements of array to zero.
+__global__ void ClearArray32(float* address) {
+  address[threadIdx.x + blockIdx.x * blockDim.x] = 0;
+}
+__global__ void ClearArray64(double* address) {
+  address[threadIdx.x + blockIdx.x * blockDim.x] = 0;
+}
+
+#endif  // _DEFINES_
