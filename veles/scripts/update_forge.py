7eb5eb27d3a94ef2a8ad53f032d8a9f2e99a4783
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-12-11 16:17:10 +0300

    Added loging into update_forge
    Change-Id: I5baae09a100092b8826bb6c26b733d8d7e2e7bd2
    
    Former-commit-id: 3a7e2ca231984a654b9a865cb5069805f1384796

--- veles/scripts/update_forge.py
+++ veles/scripts/update_forge.py
@@ -7,24 +7,29 @@ veles/scripts/update_forge.py
 """
 
 
+import logging
 import subprocess
 import os
+import sys
 
 from veles.config import root
 from veles.scripts.generate_frontend import scan_workflows
+from veles.logger import Logger
 
 
-def run(server_url):
-    workflows = scan_workflows(False)
-    for workflow in workflows:
-        workflow_folder = os.path.dirname(workflow)
-        if root.common.forge.manifest in os.listdir(workflow_folder):
-            print("Update workflow %s in VELESForge" % workflow_folder)
-            subprocess.call(
-                ["python3", "-m", "veles", "forge", "upload", "-s", server_url,
-                 "-d", workflow_folder])
-        else:
-            print("Workflow %s is not in Forge" % workflow)
+class Main(Logger):
+    def run(self, server_url):
+        workflows = scan_workflows(False)
+        for workflow in workflows:
+            workflow_folder = os.path.dirname(workflow)
+            if root.common.forge.manifest in os.listdir(workflow_folder):
+                self.info("Update workflow %s in VELESForge" % workflow_folder)
+                subprocess.call(
+                    ["python3", "-m", "veles", "forge", "upload", "-s",
+                     server_url, "-d", workflow_folder])
+            else:
+                self.info("Workflow %s is not in Forge" % workflow)
 
 if __name__ == "__main__":
-    run(os.getenv("FORGE_SERVER"))
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(Main().run(os.getenv("FORGE_SERVER")))
