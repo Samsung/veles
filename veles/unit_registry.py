07f73ba871c9e821b385aeb610e64e2bfb179baf
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-01-26 19:43:47 +0300

    Added the check for misprints in Unit.__init__
    Change-Id: Id4a635d7ce3651204b4b9c575968a1bd571af2cf
    
    Former-commit-id: 33fb038707d5898e0e5d306915c9998af98eed71

--- veles/unit_registry.py
+++ veles/unit_registry.py
@@ -6,6 +6,9 @@ Copyright (c) 2013 Samsung Electronics Co., Ltd.
 
 
 from veles.distributable import Distributable
+import inspect
+from pyxdameraulevenshtein import damerau_levenshtein_distance
+import re
 
 
 class UnitRegistry(type):
@@ -19,6 +22,7 @@ class UnitRegistry(type):
     automatically hidden.
     """
     units = set()
+    kwarg_re = re.compile(r"kwargs\.get\(([^\s,]+)|kwargs\[([^\]]+)")
 
     def __init__(cls, name, bases, clsdict):
         yours = set(cls.mro())
@@ -29,3 +33,54 @@ class UnitRegistry(type):
                 not getattr(cls, 'hide_all', False):
             UnitRegistry.units.add(cls)
         super(UnitRegistry, cls).__init__(name, bases, clsdict)
+
+    def __call__(cls, *args, **kwargs):
+        """ Checks for misprints in argument names """
+        obj = super(UnitRegistry, cls).__call__(*args, **kwargs)
+        kwattrs = set()
+        for base in cls.__mro__:
+            try:
+                src, _ = inspect.getsourcelines(base.__init__)
+            except TypeError:
+                continue
+            for line in src:
+                # IUnit requires kwargs to be named as "kwargs", so apply
+                # a simple regular expression here
+                match = UnitRegistry.kwarg_re.search(line)
+                if match is not None:
+                    kwattrs.add((match.group(1) or match.group(2))[1:-1])
+        cls.KWATTRS = kwattrs
+        # Build the matrix of differences
+        matrix = {}
+        matched = set()
+        for given_kwarg in kwargs:
+            for kwattr in kwattrs:
+                if (kwattr, given_kwarg) in matrix:
+                    continue
+                matrix[(given_kwarg, kwattr)] = d = \
+                    damerau_levenshtein_distance(given_kwarg, kwattr)
+                if d == 0:
+                    # perfect match, stop further comparisons
+                    matched.add(given_kwarg)
+                    break
+        if len(matched) < len(kwargs):
+            # Find replacement candidates with distance = 1
+            ignored_kwargs = []
+            for given_kwarg in set(kwargs).difference(matched):
+                candidates = []
+                for kwattr in kwattrs:
+                    d = matrix.get((given_kwarg, kwattr))
+                    if d == 1:
+                        candidates.append(kwattr)
+                if len(candidates) == 0:
+                    ignored_kwargs.append(given_kwarg)
+                else:
+                    obj.warning(
+                        "Creating %s: potential misprint in keyword argument "
+                        "name: expected %s - got %s", obj,
+                        " or ".join(candidates), given_kwarg)
+            if len(ignored_kwargs) > 0:
+                obj.warning(
+                    "Creating %s: ignored the following keyword arguments: %s",
+                    obj, ", ".join(ignored_kwargs))
+        return obj
