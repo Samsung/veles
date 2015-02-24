ba53ee737cf3eb17a37422040e40e501602f45d1
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-02-24 14:04:11 +0300

    Added Twisted reactor support
    Change-Id: I08bda5f8732f69aa657be2bb773e3b8cae002970
    
    Former-commit-id: b467a8de6efa7fa3046bbb30ddedd622af29e018

--- veles/external/progressbar/spinner.py
+++ veles/external/progressbar/spinner.py
@@ -11,6 +11,10 @@ from itertools import cycle
 from sys import stdout
 from time import clock
 from threading import Lock
+try:
+    from twisted.internet import reactor
+except ImportError:
+    reactor = None
 
 
 __symbols = cycle(('|\r', '/\r', '—\r', '\\\r'))
@@ -20,10 +24,13 @@ __last_flush_time = clock()
 
 def spin(interval=0.2):
     with __lock:
-        time = clock()
-        global __last_flush_time
-        if time - __last_flush_time < interval:
-            return
-        __last_flush_time = time
         stdout.write(next(__symbols))
-        stdout.flush()
+        if reactor is None or not reactor.running:
+            time = clock()
+            global __last_flush_time
+            if time - __last_flush_time < interval:
+                return
+            stdout.flush()
+            __last_flush_time = time
+        else:
+            reactor.callFromThread(stdout.flush)
