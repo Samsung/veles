ccd2f8c39762de70df58058c6c2c28fb22638f58
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-03-23 13:12:46 +0300

    Removed garbage assertion on Python 2, added print_cycles()
    Change-Id: If0409f70b5e0e532de642a0720af79f809eca2b6
    
    Former-commit-id: fd4bff5cbf6ba2103bc23c8d156b37d21e4fe887

--- veles/tests/accelerated_test.py
+++ veles/tests/accelerated_test.py
@@ -9,6 +9,8 @@ import gc
 import inspect
 import logging
 from six import PY3
+import sys
+from types import FrameType
 import unittest
 
 from veles.backends import Device, BackendRegistry
@@ -68,7 +70,8 @@ class AcceleratedTest(unittest.TestCase, Logger):
             Vector.reset_all()
         del self.device
         gc.collect()
-        assert len(gc.garbage) == 0
+        if PY3:
+            assert len(gc.garbage) == 0, str(gc.garbage)
 
     def run(self, result=None):
         failure = getattr(type(self), "__unittest_expecting_failure__", False)
@@ -83,3 +86,65 @@ class AcceleratedTest(unittest.TestCase, Logger):
     def main():
         logging.basicConfig(level=logging.DEBUG)
         unittest.main()
+
+
+def print_cycles(objects, out=sys.stdout, show_progress=False):
+    """
+    objects:       A list of objects to find cycles in.  It is often useful
+                   to pass in gc.garbage to find the cycles that are
+                   preventing some objects from being garbage collected.
+    out:     The stream for output.
+    show_progress: If True, print the number of objects reached as they are
+                   found.
+    """
+
+    def print_path(path):
+        for i, step in enumerate(path):
+            # next "wraps around"
+            nextobj = path[(i + 1) % len(path)]
+
+            out.write("   %s -- " % type(step))
+            if isinstance(step, dict):
+                for key, val in step.items():
+                    if val is nextobj:
+                        out.write("[%s]" % key)
+                        break
+                    if key is nextobj:
+                        out.write("[key] = %s" % val)
+                        break
+            elif isinstance(step, (list, tuple)):
+                if nextobj not in step:
+                    out.write(str(nextobj))
+                else:
+                    out.write("[%d] %s" % (step.index(nextobj), nextobj))
+            else:
+                out.write(repr(step))
+            out.write(" ->\n")
+        out.write("\n")
+
+    def recurse(head, start, visited, current_path):
+        if show_progress:
+            out.write("%d\r" % len(visited))
+
+        visited.add(id(head))
+
+        referents = gc.get_referents(head)
+        for referent in referents:
+            # If we've found our way back to the start, this is
+            # a cycle, so print it out
+            if referent is start:
+                print_path(current_path)
+
+            # Don't go back through the original list of objects, or
+            # through temporary references to the object, since those
+            # are just an artifact of the cycle detector itself.
+            elif referent is objects or isinstance(referent, FrameType):
+                continue
+
+            # We haven't seen this object before, so recurse
+            elif id(referent) not in visited:
+                recurse(referent, start, visited, current_path + [head])
+
+    for obj in objects:
+        out.write("Examining: %r\n" % obj)
+        recurse(obj, obj, set(), [])
