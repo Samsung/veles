8b52dbb9a2178b63d6163a10995b9662daa8aeda
Alexey Kazantsev <a.kazantsev@samsung.com>
2015-03-23 17:59:32 +0300

    Added thread pool destroy at the end of the AcceleratedTest
    Change-Id: I9cec76153f9a94e366d5ee35c3cb0872894acc92
    
    Former-commit-id: 7d0818641da60d64cbbf15d4c859c31f0e31062f

--- veles/tests/accelerated_test.py
+++ veles/tests/accelerated_test.py
@@ -20,6 +20,7 @@ from veles.logger import Logger
 if PY3:
     from veles.memory import Vector
 from veles.opencl_types import dtypes
+from veles.units import Unit
 
 
 def multi_device(numpy=False):
@@ -75,6 +76,8 @@ class AcceleratedTest(unittest.TestCase, Logger):
         pass
 
     def tearDown(self):
+        Unit._pool_.shutdown()
+        Unit._pool_ = None
         if PY3:
             Vector.reset_all()
         if sys.exc_info() == (None,) * 3:
