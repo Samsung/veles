bccc5da110e9314bef59d73bc5c90f5f8f9d785a
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-08-14 15:00:51 +0300

    Applied Apache license
    Change-Id: I5b0b2d9744475566bf93c6f60fea4af585cd6862

--- libVeles/src/unit.cc
+++ libVeles/src/unit.cc
@@ -7,7 +7,25 @@
  *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
  *
  *  @section Copyright
- *  Copyright 2013 Samsung R&D Institute Russia
+ *  Copyright © 2013 Samsung R&D Institute Russia
+ *
+ *  @section License
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 
 
