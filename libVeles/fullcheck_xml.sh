f2c190b8fe918257a4b166eaa6bd929fee7339fe
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 19:02:05 +0800

    Delete libVeles directory

--- libVeles/fullcheck_xml.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./rapidjson/*" -not -path "./variant/*" -not -path "./eina/*" -not -path "./inc/veles/eina_*" -not -path "*/libarchive/*" -not -path "./zlib/*" -not -name "config.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
--- libVeles/src/Sources.make
+++
@@ -1,3 +0,0 @@
-SOURCES := unit.cc unit_factory.cc workflow.cc workflow_loader.cc logger.cc \
-numpy_array_loader.cc main_file_loader.cc iarchivestream.cc workflow_archive.cc \
-engine.cc memory_optimizer.cc packaged_numpy_array.cc
