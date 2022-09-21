f9f18a47e2fcc3d108aefbc2e5272b2b2f0d24c7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 19:01:23 +0800

    Delete gource directory

--- gource/gource-logger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-base=$3/
-path=$1/$2
-path=${path/$base/}
-sed_path=${path//\//\\\/}
-if [[ $path != "libVeles/libarchive" && $path != "libVeles/zlib" && $path != "libSoundFeatureExtraction/DSPFilters" && $path != "Znicz/libZnicz/simd" ]]; then
-git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | sed -r -e "s/(\.\.\. \w\s+)/\1$sed_path\//g" -e 's/:[^A-Z]+(\w)\s+(.*)/|\1|\2/g'
-fi
